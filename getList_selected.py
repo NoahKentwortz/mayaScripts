222195853c7ad758d6e737838c872a6835e5a20d
FischerMoseley <fischerm@mit.edu>
2019-08-09 20:58:50 -0600

    Update getList_selected.py

--- getList_selected.py
+++ getList_selected.py
@@ -1,8 +1,8 @@
 import maya.cmds as cmds 
 
 sad_nuggets = cmds.ls(sl=True,long=True) #cmds.ls listing sl(selected DAG objects), long(the full names of the objects)
-ascii_encoded = [str(nugget) for nugget in sad_nuggets]
-#sadder_nuggets = [sad_nuggets.Replace("u'|", "")] 
+ascii_encoded = [str(nugget) for nugget in sad_nuggets] #re-encodes to ascii from unicode; don't use non-ascii characters in your object names
+ascii_encoded_and_cleaned = [x[1:] for x in ascii_encoded] #removes the first character, regardless of what it is (in this case it should be a | )
 
-print sad_nuggets
 print ascii_encoded
+print ascii_encoded_and_cleaned
