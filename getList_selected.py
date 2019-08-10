652a87d3be5c7f89f06d6f4688cdca415d8f8cbd
FischerMoseley <fischerm@mit.edu>
2019-08-09 20:45:11 -0600

    Convert unicode to ascii strings

--- getList_selected.py
+++ getList_selected.py
@@ -1,6 +1,8 @@
 import maya.cmds as cmds 
 
 sad_nuggets = cmds.ls(sl=True,long=True) #cmds.ls listing sl(selected DAG objects), long(the full names of the objects)
+ascii_encoded = [str(nugget) for nugget in sad_nuggets]
 #sadder_nuggets = [sad_nuggets.Replace("u'|", "")] 
 
 print sad_nuggets
+print ascii_encoded
